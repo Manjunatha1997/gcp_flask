350124bb43e387db66dfb84c87a80cbacd3ec4b0
Manjunatha1997 <mr.papireddigari@gmail.com>
2026-05-13 20:26:02 +0530

    first push

---
+++ main.py
@@ -0,0 +1,19 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/about")
+def about():
+    return "This is a simple Flask app."
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",  # Allow external access
+        port=5000,
+        debug=True       # Enable debug mode
+    )
