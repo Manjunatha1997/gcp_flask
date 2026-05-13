c7e7a9206a29f729e9d86c80dd93c771d127f500
Manjunatha1997 <mr.papireddigari@gmail.com>
2026-05-13 22:49:54 +0530

    updated main file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Gunicorn + Flask running successfully on GCP"
+        "message": "Flask running successfully on GCP"
     })
 
 @app.route("/health")
